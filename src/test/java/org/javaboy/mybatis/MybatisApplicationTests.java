9c23a667fe916e6005c2125104c7a307fb3c5d81
86178 <1207350458@qq.com>
2020-03-10 17:01:42 +0800

    初始化

---
+++ src/test/java/org/javaboy/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
